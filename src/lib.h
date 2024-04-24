32a90a00d01431ddd76d423f6d0789cbb229f815
Fernando Pires <ferjpires@gmail.com>
2024-04-24 14:55:17 +0100

    struct inserted

---
+++ src/lib.h
@@ -0,0 +1,17 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+typedef struct
+{
+    int running; // 1 for running, 0 for done
+    int time;
+    char flag[3];
+    char arguments[300];
+    pid_t processID;
+} program;
+
+#endif
