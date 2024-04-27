2a9da715f2e9915aca67aca41085c812e9cab9dd
a103998 <a103998@alunos.uminho.pt>
2024-04-27 19:04:03 +0100

    orchestrator advance

--- src/orchestrator.c
+++ src/orchestrator.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <lib.h>
+
+#define FIFO_PATH "/src/TUBO"
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!\n");
+   
+   char exec_args[20][300];
+   Program programa;
+
+    int fd = open(FIFO_PATH, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    int results = open("results.txt", O_RDWR | O_CREAT | O_APPEND);
+
+    while (read(fd,&programa,sizeof(programa)) > 0)
+    {
+        parseArguments(programa,exec_args);
+
+        int id = fork();
+
+        if (id == 0) //Child Process
+        {
+            dup2(results,1);
+            execvp(1,exec_args);
+            _exit(1);
+        }
+        else //Parent Process
+        {
+            wait(NULL);
+        }
+        sleep(3);
+    }
+
     return 0;
 }
