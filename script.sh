b0cc1ede7a35ec1f2a4a6e6235c44c349262c652
ferjpires <ferjpires@gmail.com>
2024-05-07 20:17:50 +0100

    changed the number of commands sent by script

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ enviar_pedido()
     "$CLIENTE" "${args[@]}"
 }
 
-for ((i=1; i<=100; i++)); do
+for ((i=1; i<=200; i++)); do
     if [ $((i % 50)) -eq 0 ]; then
         enviar_pedido "execute" "10000" "-u" "sleep 10"
     elif [ $((i % 4)) -eq 0 ]; then
